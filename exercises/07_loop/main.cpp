a31379c9eaf3b5ba4b3e37bc565d4e579ec16e23
bepersevering <mingshashan@gmail.com>
2025-01-04 20:21:50 +0800

    exercises 08

--- exercises/07_loop/main.cpp
+++ exercises/07_loop/main.cpp
@@ -5,9 +5,14 @@
 // READ: 纯函数 <https://zh.wikipedia.org/wiki/%E7%BA%AF%E5%87%BD%E6%95%B0>
 static unsigned long long fibonacci(int i) {
     // TODO: 为缓存设置正确的初始值
-    static unsigned long long cache[96], cached;
+    static unsigned long long cache[96];
+    static int cached;
+    cache[0] = 0;
+    cache[1] = 1;
+    cache[2] = 1;
+    cache[3] = 2;
     // TODO: 设置正确的循环条件
-    for (; false; ++cached) {
+    for (cached = 2; cached <= i; ++cached) {
         cache[cached] = cache[cached - 1] + cache[cached - 2];
     }
     return cache[i];
