7d926984c88290e58d31158b026b627281738f3e
bepersevering <mingshashan@gmail.com>
2025-01-05 18:10:32 +0800

    exercises 08

--- exercises/08_pointer/main.cpp
+++ exercises/08_pointer/main.cpp
@@ -1,10 +1,19 @@
 ﻿#include "../exercise.h"
+#include <algorithm>
 
 // READ: 数组向指针退化 <https://zh.cppreference.com/w/cpp/language/array#%E6%95%B0%E7%BB%84%E5%88%B0%E6%8C%87%E9%92%88%E7%9A%84%E9%80%80%E5%8C%96>
 bool is_fibonacci(int *ptr, int len, int stride) {
     ASSERT(len >= 3, "`len` should be at least 3");
     // TODO: 编写代码判断从 ptr 开始，每 stride 个元素取 1 个元素，组成长度为 n 的数列是否满足
     // arr[i + 2] = arr[i] + arr[i + 1]
+
+    int i = 0;
+    for (; i + 2 < len; ++i) {
+        if (ptr[(i+2)*stride] != (ptr[i*stride] + ptr[(i+1)*stride])) {
+            return false;
+        }
+    }
+
     return true;
 }
 
@@ -21,6 +30,9 @@ int main(int argc, char **argv) {
     ASSERT(!is_fibonacci(arr2    , sizeof(arr2) / sizeof(*arr2)    , 1),         "arr2 is not Fibonacci");
     ASSERT( is_fibonacci(arr2 + 2, 10                              , 2), "part of arr2 is Fibonacci"    );
     ASSERT( is_fibonacci(arr2 + 3,  9                              , 2), "part of arr2 is Fibonacci"    );
+    std::cout 
+    << "#####"
+    <<!is_fibonacci(arr2 + 3, 10                              , 2) <<std::endl;
     ASSERT(!is_fibonacci(arr2 + 3, 10                              , 2), "guard check"                  );
     ASSERT(!is_fibonacci(arr2 + 1, 10                              , 2), "guard check"                  );
     // clang-format on
