4fd4f1820d31f8ff73d9e28078da145f10acc265
bepersevering <mingshashan@gmail.com>
2025-01-12 22:42:18 +0800

    exercises 20

--- exercises/20_function_template/main.cpp
+++ exercises/20_function_template/main.cpp
@@ -1,9 +1,18 @@
 ﻿#include "../exercise.h"
+#include <algorithm>
 
 // READ: 函数模板 <https://zh.cppreference.com/w/cpp/language/function_template>
 // TODO: 将这个函数模板化
-int plus(int a, int b) {
+// int plus(int a, int b) {
+//     return a + b;
+// }
+
+template<typename T> T plus(T a, T b) {
     return a + b;
+} 
+
+bool is_equal(double a, double b, double epsilon = 1e-9) {
+    return std::abs(a - b) < epsilon;
 }
 
 int main(int argc, char **argv) {
@@ -11,10 +20,10 @@ int main(int argc, char **argv) {
     ASSERT(plus(1u, 2u) == 3u, "Plus two unsigned int");
 
     // THINK: 浮点数何时可以判断 ==？何时必须判断差值？
-    ASSERT(plus(1.25f, 2.5f) == 3.75f, "Plus two float");
-    ASSERT(plus(1.25, 2.5) == 3.75, "Plus two double");
+    ASSERT(is_equal(plus(1.25f, 2.5f), 3.75f), "Plus two float");
+    ASSERT(is_equal(plus(1.25, 2.5), 3.75), "Plus two double");
     // TODO: 修改判断条件使测试通过
-    ASSERT(plus(0.1, 0.2) == 0.3, "How to make this pass?");
+    ASSERT(is_equal(plus(0.1, 0.2), 0.3), "How to make this pass?");
 
     return 0;
 }
